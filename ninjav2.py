dd0ab3b8fbe598e9b55428a26c4ba669449f4014
MR_Ninja <54943600+KuntuL397@users.noreply.github.com>
2019-10-07 19:41:15 +0800

    Add files via upload

---
+++ ninjav2.py
@@ -0,0 +1,18 @@
+#!bin /bash/usr
+ 
+ 
+ then
+ clear
+ echo 'ELLO WORLD'
+ echo 'welcome'
+ txt1 '$username' 'MR NINJA'
+ echo '$p' '$txt' 'halo '$username' 
+ fi
+ 
+ 
+ if 
+ echo
+ echo '$p' 'makasih ya kentod:v'
+ fi
+ 
+ 
